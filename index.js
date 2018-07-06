5dc6bd00871d6f409026f954e8c046f56f48eb3e
agamjain14 <agamjain14@gmail.com>
2018-07-07 00:11:12 +0530

    register-api

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// 4) authentication
+const router = express.Router();
+const authentication = require('./routes/authentication')(router);
+
+// 5) body parser
+
+const bodyParser = require('body-parser');
+
 // 2) Mongodb connection
 var mongoose = require('mongoose');
 const config = require('./config/database');
@@ -14,14 +22,21 @@ mongoose.connect(config.uri, (err) => {
     }
 });
 
+// 5) put bodyparser code before the routes (converts request to req.body)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // 3) connect node.js to angular 2
-
-
 app.use(express.static(__dirname + '/client/dist/client/'));
 
+// 4) 
+app.use('/authentication', authentication);
+// 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
+
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
